0dee324684a858b3a72d6b90c5d45cc2e18c98ef
NiklassssHartmannnnn <Niklshartmann@gmail.com>
2026-03-11 18:32:15 +0100

    Added the base domain classes for exercises. They are anemic for now

---
+++ src/backend/Modules/Exercise/Exercise-Domain/Entities/Keyword.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise_Domain.Entities
+{
+    public class Keyword
+    {
+        public Guid Id { get; private set; }
+
+        public string KeywordName { get; private set; }
+
+        public KeywordType KeywordType { get; private set; }
+
+        private Keyword() { }
+
+        public Keyword(string keywordName, KeywordType keywordType)
+        {
+            Id = Guid.NewGuid();
+            KeywordName = keywordName;
+            KeywordType = keywordType;
+        }
+    }
+
+    public enum KeywordType
+    {
+        SchoolSubject,
+        ExerciseType,
+        SubjectArea
+    }
+}
