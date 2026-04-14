c4b3555a73820e095d848b65385a9c20025ef4f7
NiklassssHartmannnnn <Niklshartmann@gmail.com>
2026-04-14 18:31:30 +0200

    Began working on Assignment domain classes, need to discuss things.

--- Assign/Assignment-Domain/Entities/Assignment.cs
+++ Assign/Assignment-Domain/Entities/Assignment.cs
@@ -9,10 +9,19 @@ namespace Assignment_Domain.Entities
 {
     public class Assignment : Entity
     {
+        public string Title{ get; private set; } //Tilføjet, mangler at komme på klasse diagrammet
+        public DateTime CreatedAt { get; private set; } //Tilføjet, mangler at komme på klasse diagrammet
 
+        public IReadOnlyCollection<AssignmentExercise> AssignmentExercises => _assignmentExercises;
+        private readonly List<AssignmentExercise> _assignmentExercises = new List<AssignmentExercise>();
 
-        public Assignment() : base(Guid.NewGuid())
-        {
-        }
+        public IReadOnlyCollection<SubmittedAssignment> SubmittedAssignments => _submittedAssignments;
+        private readonly List<SubmittedAssignment> _submittedAssignments = new List<SubmittedAssignment>();
+
+        private Assignment() : base(Guid.NewGuid()) { }
+
+        //Skal vi have nogle flere attributter?
+        // / Skal vi have en CreatedByTeacherId ligesom i Exercise?
+        // Skal vi have en type, eksempelvis med eller uden hjælpemidler?
     }
 }
